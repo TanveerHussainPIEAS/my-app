17fd574c85e97e1b789205c0404f0474d8a50ed0
Tanveer Hussain <thussain@cinnova.com>
2023-03-14 17:02:02 +0500

    Components Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './Componets/Greet';
+import Welcome from './Componets/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name='Tanveer'></Greet>
+      <Greet name='Ali'></Greet>
+      <Welcome></Welcome>
     </div>
   );
 }
---
+++ src/Componets/Greet.js
@@ -0,0 +1,6 @@
+
+import React from "react";
+
+const Greet= (props)=><h1>Hi {props.name}</h1>
+
+export default Greet;
