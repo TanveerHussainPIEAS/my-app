662342bff7832c84533a5725423a1d2e8fe519e1
Tanveer Hussain <thussain@cinnova.com>
2023-03-17 15:49:24 +0500

    Material UI Added

--- src/Componets/ChildComponent.js
+++ src/Componets/ChildComponent.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
+import Button from '@material-ui/core/Button';
 
 export class ChildComponent extends Component {
-  render() {
-    return (
-      <div>
-        <button onClick={()=>this.props.greetHandeler('Child-1')}>Call to Parent</button>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Button variant="contained" color="primary" onClick={() => this.props.greetHandeler('Child-1')}>
+                Call to Parent
+                </Button>
+            </div>
+        )
+    }
 }
 
 export default ChildComponent
--- src/Componets/Counter.js
+++ src/Componets/Counter.js
@@ -1,38 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
 export class Counter extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         count:0
-      }
-    }
+  constructor(props) {
+    super(props)
 
-    IncreaseValue(){
-        this.setState({
-            count:this.state.count+1
-        },
-        ()=>{
-            console.log('Value in callback function', this.state.count)
-        })
-        console.log('Async time value', this.state.count)
+    this.state = {
+      count: 0
     }
-    DecreaseValue(){
-        this.setState({
-            count:this.state.count-1
-        },
-        ()=>{
-            console.log('Value in callback function', this.state.count)
-        })
-        console.log('Async time value', this.state.count)
-    }
+  }
+
+  IncreaseValue() {
+    this.setState({
+      count: this.state.count + 1
+    },
+      () => {
+        console.log('Value in callback function', this.state.count)
+      })
+    console.log('Async time value', this.state.count)
+  }
+  DecreaseValue() {
+    this.setState({
+      count: this.state.count - 1
+    },
+      () => {
+        console.log('Value in callback function', this.state.count)
+      })
+    console.log('Async time value', this.state.count)
+  }
   render() {
     return (
       <div>
-      <div>Counter - {this.state.count}</div>
-      <div><button onClick={()=>this.IncreaseValue()}>Increment</button></div>
-      <div><button onClick={()=>this.DecreaseValue()}>Decrement</button></div>
+        <div>Counter - {this.state.count}</div>
+        <div>
+          <Button variant="contained" color="primary" onClick={() => this.IncreaseValue()}>Increment </Button>
+        </div>
+
+        <div>
+          <Button variant="contained" color="primary" onClick={() => this.DecreaseValue()}>Decrement </Button>
+        </div>
       </div>
     )
   }
