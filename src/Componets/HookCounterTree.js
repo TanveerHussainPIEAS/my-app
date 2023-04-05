e77243f24ce8c43f858487ea322751b0e8ec5300
Tanveer Hussain <thussain@cinnova.com>
2023-04-05 12:10:43 +0500

    ..

--- src/Componets/HookCounterTree.js
+++ src/Componets/HookCounterTree.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react'
 import TextField from '@mui/material/TextField';
-import { json } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 function HookCounterTree() {
     const [name, setName]=useState({firstName: '', lastName:''})
+    const [items,setItems]= useState([])
+
+    const addItem=()=>{
+        setItems([...items,{
+            id: items.length,
+            value: Math.floor(Math.random()*10+1)
+        }])
+    }
     return (
         <div>
             <h1>React Hooks with Object</h1>
@@ -20,6 +28,17 @@ function HookCounterTree() {
                 </div>
                 <div>
                     {JSON.stringify(name)}
+                </div>
+                <div>
+                    <h1>Modify array in a state</h1>
+                </div>
+                <div>
+                <Button  variant="contained" color="primary" onClick={()=>addItem()}>Add Item </Button>
+                </div>
+                <div>
+                    <ol>
+                        {items.map(item=> <li key={item.id}>{item.value}</li>)}
+                    </ol>
                 </div>
             </form>
         </div>
